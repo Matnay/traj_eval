c87c0812a17f81daa479294837980a4ce93cf004
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2020-08-15 23:39:34 +0530

    pose graph optimised path
    Added functionality to check for pose graph optimized trajectory

---
+++ src/loop_closure.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Int32.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+geometry_msgs::PoseStamped ground_odom;
+nav_msgs::Path current_odom;
+using namespace std;
+void pose_stamped_callback(const geometry_msgs::PoseStamped& msg)
+{
+  ground_odom=msg;
+          std::ofstream out;
+        out.open("stamped_groundtruth.txt", std::ios_base::app);
+        out<<ros::Time::now()<<" "<<ground_odom.pose.position.x<<" "<<ground_odom.pose.position.y<<" "<<ground_odom.pose.position.z<<" "<<ground_odom.pose.orientation.x<<" "<<ground_odom.pose.orientation.y<<" "<<ground_odom.pose.orientation.z<<" "<<ground_odom.pose.orientation.w<<endl;
+        out.close();
+} 
+
+void odom_callback(const nav_msgs::Path& msg)
+{
+  current_odom=msg;
+          std::ofstream outfile;
+        outfile.open("stamped_pose_graph_estimate.txt", std::ios_base::app);
+        outfile<<ros::Time::now()<<" "<<current_odom.poses[0].pose.position.x<<" "<<current_odom.poses[0].pose.position.y<<" "<<current_odom.poses[0].pose.position.z<<" "<<current_odom.poses[0].pose.orientation.x<<" "<<current_odom.poses[0].pose.orientation.y<<" "<<current_odom.poses[0].pose.orientation.z<<" "<<current_odom.poses[0].pose.orientation.w<<endl;
+        outfile.close();
+} 
+
+
+int main(int argc, char** argv) {
+  ros::init(argc,argv,"relative_pose");
+  ros::NodeHandle nh;
+      ros::Subscriber ground_truth_odom=nh.subscribe("/blackbird/pose_ref",1,pose_stamped_callback);
+      ros::Subscriber odom=nh.subscribe("pose_graph/path_1",1,odom_callback);
+      ros::spin();
+}
